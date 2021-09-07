340ed55f3701c1f1207c47f53f5c319b95f31825
takakazu2000 <55226775+takakazu2000@users.noreply.github.com>
2021-09-07 16:39:40 +0900

    update

---
+++ PotRotate.hpp
@@ -0,0 +1,19 @@
+#ifndef PotRotate_HPP_
+#define PotRotate_HPP_
+
+const float PI = 3.14159f;
+float r = 1; //回転テーブルの中心からポットの中心までの距離(mm)
+
+class PotRotate
+{
+protected:
+
+
+private:
+    void potrotation(float*, float*, float);
+    
+public:
+
+};
+
+#endif // !PotRotate_HPP_
