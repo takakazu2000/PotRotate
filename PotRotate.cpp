dab99ba9b1b63abdb3f67e648b8829fadcb1cb57
takakazu2000 <55226775+takakazu2000@users.noreply.github.com>
2021-09-16 17:41:15 +0900

    update

--- PotRotate.cpp
+++ PotRotate.cpp
@@ -7,16 +7,3 @@ void potrotation(float* x, float* y, float rotation)
     *x = *x + r * cos(rotation);
     *y = *y + r * sin(rotation);
 }
-
-int  main(){
-    float x = 10;
-    float y = 10;
-    float rotation = 30 * PI / 180;
-    potrotation(&x,&y,rotation);
-
-    for( int i=0; i<10; i++ ){
-        printf("x:%f.2, y:%f.2\r\n",x,y);
-    }
-
-    return 0;
-} 
